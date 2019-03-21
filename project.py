5ec9dc4ab54b08794bd09b9f9c73ce7d72d45cef
Jack <jack.fw.holtby@gmail.com>
2019-03-21 21:10:40 +0800

    Fixed up variables for basic routing setup. Wow.

--- project.py
+++ project.py
@@ -14,7 +14,7 @@ session = DBSession()
 
 # Show main Catalogue page
 @app.route('/')
-@app.route('/catalogue')
+@app.route('/catalogue/')
 def showCatalogue():
     output = "<p>The Catalogue's main page</p>"
     return output
@@ -23,33 +23,39 @@ def showCatalogue():
 @app.route('/catalogue/<int:category_id>/')
 @app.route('/catalogue/<int:category_id>/items/')
 def showCategory(category_id):
-    category = session.query(Category).filter_by(id = category_id).one()
-    return "The page for category with id: %s" % category
+    category = session.query(Category).filter_by(id=category_id).one()
+    return "The page for category with id: %s" % category.id
 
 # Edit a category
 @app.route('/catalogue/<int:category_id>/edit/')
 def editCategory(category_id):
-    return "The page for editing category with id: %s" % category_id
+    category = session.query(Category).filter_by(id=category_id).one()
+    return "The page for editing category with id: %s" % category.id
 
 # Delete a category
 @app.route('/catalogue/<int:category_id>/delete/')
 def deleteCategory(category_id):
-    return "The page for deleting category with id: %s" % category_id
+    category = session.query(Category).filter_by(id=category_id).one()
+    return "The page for deleting category with id: %s" % category.id
 
 # Show item
 @app.route('/catalogue/<int:category_id>/<int:item_id>/')
 def showItem(item_id):
-    return "The page for showing item with id: %s" % item_id
+    category = session.query(Category).filter_by(id=category_id).one()
+    item = session.query(Item).filter_by(cat_id=category.id, id=item_id).one()
+    return "The page for showing item with id: %s" % item.id
 
 # Edit item
 @app.route('/catalogue/<int:category_id>/<int:item_id>/edit/')
 def editItem(item_id):
-    return "The page for editing item with id: %s" % item_id
+    category = session.query(Category).filter_by(id=category_id).one()
+    return "The page for editing item with id: %s" % item.id
 
 # Delete item
 @app.route('/catalogue/<int:category_id>/<int:item_id>/delete/')
 def deleteItem(item_id):
-    return "The page for deleting item with id: %s" % item_id
+    category = session.query(Category).filter_by(id=category_id).one()
+    return "The page for deleting item with id: %s" % item.id
 
 @app.route('/catalogue.json')
 def json():
