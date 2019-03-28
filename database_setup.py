3c0c845247e9b247a1038ca8332eaa715bf18bcc
Jack <jack.fw.holtby@gmail.com>
2019-03-28 21:26:54 +0800

    Added basic jsonification.

--- database_setup.py
+++ database_setup.py
@@ -13,6 +13,13 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+        return {
+        'id': self.id,
+        'name': self.name
+        }
+
 class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
@@ -20,6 +27,15 @@ class Item(Base):
     title = Column(String(100), nullable=False)
     cat_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+        'id': self.id,
+        'description': self.description,
+        'title': self.title,
+        'cat_id': self.cat_id
+        }
 
 engine = create_engine('sqlite:///categories.db')
 
