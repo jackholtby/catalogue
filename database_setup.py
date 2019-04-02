8d94812768c2fb17d4574842af4af71968cf5479
Jack <jack.fw.holtby@gmail.com>
2019-04-02 21:25:31 +0800

    Added comments to database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -8,12 +8,17 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class User(Base):
+    """
+    Registered User information is stored in this table
+    """
+
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(400), nullable=False)
     picture = Column(String(250))
 
+    # JSON format for the data.
     @property
     def serialize(self):
         return {
@@ -24,21 +29,32 @@ class User(Base):
     }
 
 class Category(Base):
+    """
+    Category information is stored here.
+    """
+
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     items = relationship("Item")
 
+
+    # JSON format for the data.
     @property
     def serialize(self):
         return {
         'id': self.id,
         'name': self.name,
+        # Loop through all items found within category.
         'items': [item.serialize for item in self.items]
         }
 
 class Item(Base):
+    """
+    Pre-generated and newly added items are stored in this table.
+    """
+
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
     description = Column(String(400), nullable=False)
@@ -48,6 +64,7 @@ class Item(Base):
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
+    # JSON format for the data.
     @property
     def serialize(self):
         return {
